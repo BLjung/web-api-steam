440bf0006fee47bc4df721eba7b23412c2062dd8
BLjung <74122179+BLjung@users.noreply.github.com>
2021-09-03 18:51:06 +0200

    Update getOwnedGames.js

--- modules/getOwnedGames.js
+++ modules/getOwnedGames.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const tjs = require('./typeof');
 
-module.exports = (steam64id, apikey, freeGames, callback) => {
+module.exports = (steam64id, apikey, freeGames, appInfo callback) => {
   if(tjs(steam64id) && tjs(apikey) !== undefined){
     request({
       uri: '/IPlayerService/GetOwnedGames/v1/',
@@ -9,7 +9,8 @@ module.exports = (steam64id, apikey, freeGames, callback) => {
       qs: {
         key: apikey,
         steamid: steam64id,
-        include_played_free_games: freeGames
+        include_played_free_games: freeGames,
+        include_appinfo: appInfo
       }
     }, (err, res, body) => {
       if(err) callback("There was an error making your request.");
